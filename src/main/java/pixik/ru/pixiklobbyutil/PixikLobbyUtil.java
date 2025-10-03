bd17f9debb404d510d37d1e19064c657aef739fd
PixikDev <ilnazxuligan@gmail.com>
2025-10-03 20:32:34 +0300

    first commit

---
+++ src/main/java/pixik/ru/pixiklobbyutil/PixikLobbyUtil.java
@@ -0,0 +1,17 @@
+package pixik.ru.pixiklobbyutil;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PixikLobbyUtil extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
